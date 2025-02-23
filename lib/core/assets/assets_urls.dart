4ee3104cfb89dbb5a176075ce2d2d9a255d774e1
Vinícius Inácio <vinicius.silva@grupobrisanet.com.br>
2025-02-22 22:45:21 -0300

    chore: assets and style guid export files

---
+++ lib/core/assets/assets_urls.dart
@@ -0,0 +1,5 @@
+const SEARCH_ICON = "lib/core/assets/search_icon.svg";
+const BELL_ICON = "lib/core/assets/bell.svg";
+const ARROW_DOWN_ICON = "lib/core/assets/arrow-down.svg";
+const ARROW_UP_ICON = "lib/core/assets/arrow-up.svg";
+const CIRCLE_ICON = "lib/core/assets/circle-icon.svg";
--- lib/core/style_guide/style_guide.dart
+++ lib/core/style_guide/style_guide.dart
@@ -1,2 +1,1 @@
 export './app_colors.dart';
-export './shadows.dart';
